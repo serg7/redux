8f8059db8be602c21f164888bfd5c6a81741de2c
Sergey <serhiyborsuk@gmail.com>
2017-04-02 11:56:11 +0300

    create root and course reducers

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,9 +1,10 @@
 export default function courseReducer(state = [], action) {
     switch(action.type) {
         case 'CREATE_COURSE':
+            debugger;
             return [...state,
                 Object.assign({}, action.course)
-            ];  //state.push(action.course);
+            ];
 
         default:
             return state;
