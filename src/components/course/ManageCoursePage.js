247886c7d1f8ab2da6863beb802f13263d9b9380
Sergey <serhiyborsuk@gmail.com>
2017-05-31 10:45:37 +0300

    show saving status

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends React.Component {
 
         this.state = {
             course: Object.assign({}, this.props.course),
-            errors: {}
+            errors: {},
+            saving: false
         };
 
         this.updateCourseState = this.updateCourseState.bind(this);
@@ -33,7 +34,13 @@ class ManageCoursePage extends React.Component {
 
     saveCourse(event) {
         event.preventDefault();
-        this.props.actions.saveCourse(this.state.course);
+        this.setState({saving: true});
+        this.props.actions.saveCourse(this.state.course).
+            then(() => this.redirect());
+    }
+
+    redirect() {
+        this.setState({saving: false});
         this.context.router.push('/courses');
     }
 
@@ -45,6 +52,7 @@ class ManageCoursePage extends React.Component {
                     allAuthors={this.props.authors}
                     onChange={this.updateCourseState}
                     onSave={this.saveCourse}
+                    saving={this.state.saving}
                 />
             
         );
