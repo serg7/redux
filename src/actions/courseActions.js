2a5354dbc1966c5fdbedc33bb9e28c4de816201e
Sergey <serhiyborsuk@gmail.com>
2017-05-31 15:53:50 +0300

    add validation and toasts

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -32,7 +32,8 @@ export function saveCourse(course) {
         return courseApi.saveCourse(course).then(course => {
             course.id ? dispatch(updateCourseSuccess(course)) : dispatch(createCourseSuccess(course));
         }).catch(error => {
-           throw(error);
+            dispatch(ajaxCallError());
+            throw(error);
         });
 
     };
